fcabcc129e5d40148f28beb9322508fce3c8f600
Olivia Feldman <orwfeldman@gmail.com>
2023-11-22 17:28:17 +0100

    added Die component, isHeld prop, CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from "react";
-import "./index.css";
+import "./index.css"
+import Die from './Die'
+import {nanoid} from "nanoid"
+
 
 export default function App() {
-    return (
-        <main></main>
-    )
-}
+
+      const [dice, setDice] = React.useState(allNewDice())
+
+      function allNewDice() {
+          const newDice = []
+          for (let i = 0; i < 10; i++) {
+              newDice.push({
+                  value: Math.ceil(Math.random() * 6),
+                  isHeld: false,
+                  id: nanoid()
+              })
+          }
+          return newDice
+      }
+
+      function rollDice() {
+          setDice(allNewDice())
+      }
+
+      const diceElements = dice.map(die => (
+          <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+      ))
+
+      return (
+          <main>
+              <div className="dice-container">
+                  {diceElements}
+              </div>
+              <button className="roll-dice" onClick={rollDice}>Roll</button>
+          </main>
+      )
+  }
