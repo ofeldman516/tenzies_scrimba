c038c1d3ebd639a254b3e3df6989c8ff0df4fc92
Olivia Feldman <orwfeldman@gmail.com>
2023-11-22 18:24:27 +0100

    added hold dice functionality

--- src/App.js
+++ src/App.js
@@ -4,36 +4,68 @@ import Die from './Die'
 import {nanoid} from "nanoid"
 
 
+
 export default function App() {
 
-      const [dice, setDice] = React.useState(allNewDice())
+  const [dice, setDice] = React.useState(allNewDice())
 
-      function allNewDice() {
-          const newDice = []
-          for (let i = 0; i < 10; i++) {
-              newDice.push({
-                  value: Math.ceil(Math.random() * 6),
-                  isHeld: false,
-                  id: nanoid()
-              })
-          }
-          return newDice
+  function generateNewDie() {
+      return {
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false,
+          id: nanoid()
       }
-
-      function rollDice() {
-          setDice(allNewDice())
-      }
-
-      const diceElements = dice.map(die => (
-          <Die key={die.id} value={die.value} isHeld={die.isHeld} />
-      ))
-
-      return (
-          <main>
-              <div className="dice-container">
-                  {diceElements}
-              </div>
-              <button className="roll-dice" onClick={rollDice}>Roll</button>
-          </main>
-      )
   }
+
+  function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push(generateNewDie())
+      }
+      return newDice
+  }
+
+
+/**
+* Challenge: Update the `rollDice` function to not just roll
+* all new dice, but instead to look through the existing dice
+* to NOT role any that are being `held`.
+*
+* Hint: this will look relatively similiar to the `holdDice`
+* function below. When creating new dice, remember to use
+* `id: nanoid()` so any new dice have an `id` as well.
+*/
+  function rollDice() {
+      setDice(oldDice => oldDice.map(die => {
+          return die.isHeld ?
+              die :
+              generateNewDie()
+      }))
+  }
+
+  function holdDice(id) {
+      setDice(oldDice => oldDice.map(die => {
+          return die.id === id ?
+              {...die, isHeld: !die.isHeld} :
+              die
+      }))
+  }
+
+  const diceElements = dice.map(die => (
+      <Die
+          key={die.id}
+          value={die.value}
+          isHeld={die.isHeld}
+          holdDice={() => holdDice(die.id)}
+      />
+  ))
+
+  return (
+      <main>
+          <div className="dice-container">
+              {diceElements}
+          </div>
+          <button className="roll-dice" onClick={rollDice}>Roll</button>
+      </main>
+  )
+}
