f2fca5f7d6576572d9b6598d45c06956a32b61cc
Olivia Feldman <orwfeldman@gmail.com>
2023-11-29 14:12:07 +0100

    added functionality to restart game when winning

--- src/App.js
+++ src/App.js
@@ -1,71 +1,89 @@
-import React from "react";
+import React from "react"
+import Die from "./Die"
 import "./index.css"
-import Die from './Die'
 import {nanoid} from "nanoid"
-
-
+import Confetti from "react-confetti"
 
 export default function App() {
 
-  const [dice, setDice] = React.useState(allNewDice())
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
 
-  function generateNewDie() {
-      return {
-          value: Math.ceil(Math.random() * 6),
-          isHeld: false,
-          id: nanoid()
-      }
-  }
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+        }
+    }, [dice])
 
-  function allNewDice() {
-      const newDice = []
-      for (let i = 0; i < 10; i++) {
-          newDice.push(generateNewDie())
-      }
-      return newDice
-  }
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
 
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
 
 /**
-* Challenge: Update the `rollDice` function to not just roll
-* all new dice, but instead to look through the existing dice
-* to NOT role any that are being `held`.
-*
-* Hint: this will look relatively similiar to the `holdDice`
-* function below. When creating new dice, remember to use
-* `id: nanoid()` so any new dice have an `id` as well.
-*/
-  function rollDice() {
-      setDice(oldDice => oldDice.map(die => {
-          return die.isHeld ?
-              die :
-              generateNewDie()
-      }))
-  }
+ * Challenge: Allow the user to play a new game when the
+ * button is clicked and they've already won
+ */
 
-  function holdDice(id) {
-      setDice(oldDice => oldDice.map(die => {
-          return die.id === id ?
-              {...die, isHeld: !die.isHeld} :
-              die
-      }))
-  }
+    function rollDice() {
+        if(!tenzies) {
+            setDice(oldDice => oldDice.map(die => {
+                return die.isHeld ?
+                    die :
+                    generateNewDie()
+            }))
+        } else {
+            setTenzies(false)
+            setDice(allNewDice())
+        }
+    }
 
-  const diceElements = dice.map(die => (
-      <Die
-          key={die.id}
-          value={die.value}
-          isHeld={die.isHeld}
-          holdDice={() => holdDice(die.id)}
-      />
-  ))
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ?
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
 
-  return (
-      <main>
-          <div className="dice-container">
-              {diceElements}
-          </div>
-          <button className="roll-dice" onClick={rollDice}>Roll</button>
-      </main>
-  )
+    const diceElements = dice.map(die => (
+        <Die
+            key={die.id}
+            value={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+
+    return (
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same.
+            Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button
+                className="roll-dice"
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
+            </button>
+        </main>
+    )
 }
